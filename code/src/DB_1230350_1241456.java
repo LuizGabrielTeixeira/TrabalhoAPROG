45cc166e14321b107a419cace3255f9b4b598401
Bernardo Correia <superpootis12@gmail.com>
2024-11-14 14:12:19 +0000

    Created a method to create the matrix (matrixBuilder), fixed some issues in a) revolving adding a new method to create the matrix. B) is not yet working as intended

--- code/src/DB_1230350_1241456.java
+++ code/src/DB_1230350_1241456.java
@@ -2,24 +2,34 @@ import java.util.Scanner;
 
 public class DB_1230350_1241456 {
     public static void main(String[] args) {
-        planningMatrix();
+        int[][] volDeiMatrix = matrixBuilder();
+
+        planningMatrix(volDeiMatrix);
+        totalTraveledDistance(volDeiMatrix);
     }
 
-    //a)
-    public static void planningMatrix() {
+    //method that returns the matrix
+    public static int[][] matrixBuilder() {
         Scanner scanner = new Scanner(System.in);
 
         int quantityOfVehicles = scanner.nextInt();
         int quantityOfDays = scanner.nextInt();
 
         int[][] voltDeiMatrix;
-        voltDeiMatrix = new int[quantityOfVehicles][quantityOfDays+1];
+        voltDeiMatrix = new int[quantityOfVehicles][quantityOfDays + 1];
 
         for (int i = 0; i < voltDeiMatrix.length; i++) {
             for (int j = 1; j < voltDeiMatrix[i].length; j++) {
                 voltDeiMatrix[i][j] = scanner.nextInt();
             }
         }
+        return voltDeiMatrix;
+    }
+
+    //a)
+    public static void planningMatrix(int[][] voltDeiMatrix) {
+
+        int quantityOfDays = voltDeiMatrix[0].length - 1;
 
         for (int i = 0; i < quantityOfDays; i++) {
             if (i == 0) {
@@ -45,49 +55,65 @@ public class DB_1230350_1241456 {
     }
 
     //b)
-    public static void totalTraveledDistance (int numberOfRows, int[][]voltDeiMatrix){
+    public static void totalTraveledDistance(int[][] voltDeiMatrix) {
 
         int totalDistance = 0;
+        int numberOfRows = voltDeiMatrix.length;
         int[][] totalDistanceArray = new int[numberOfRows][2];
 
         for (int rowNumber = 1; rowNumber < voltDeiMatrix.length; rowNumber++) {
-            for (int columnNumber = 1; columnNumber < voltDeiMatrix[numberOfRows].length; columnNumber++) {
+            for (int columnNumber = 1; columnNumber < voltDeiMatrix[rowNumber].length; columnNumber++) {
                 totalDistance += voltDeiMatrix[rowNumber][columnNumber];
             }
 
-            totalDistanceArray[rowNumber - 1][2] = totalDistance;
+            totalDistanceArray[rowNumber - 1][1] = totalDistance;
+        }
+
+        for (int i = 0; i < totalDistanceArray.length; i++) {
+            for (int j = 0; j < totalDistanceArray[i].length; j++) {
+                System.out.println(totalDistanceArray[i][j]);
+            }
         }
     }
+
     //c)
-    public static void c (){}
+    public static void c() {
+    }
 
 
     //d)
-    public static void d (){}
+    public static void d() {
+    }
 
 
     //e)
-    public static void e (){}
+    public static void e() {
+    }
 
 
     //f)
-    public static void f (){}
+    public static void f() {
+    }
 
 
     //g)
-    public static void g (){}
+    public static void g() {
+    }
 
 
     //h)
-    public static void h (){}
+    public static void h() {
+    }
 
 
     //i)
-    public static void i (){}
+    public static void i() {
+    }
 
 
     //j)
-    public static void j (){}
+    public static void j() {
+    }
 
 
 }
