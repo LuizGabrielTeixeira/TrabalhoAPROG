fc2c47254f8e8ba707703b6fc0078f21ca50a92d
Bernardo Correia <superpootis12@gmail.com>
2024-11-14 10:52:24 +0000

    b) finished, not tested

--- code/src/DB_1230350_1241456.java
+++ code/src/DB_1230350_1241456.java
@@ -5,8 +5,8 @@ public class DB_1230350_1241456 {
         planningMatrix();
     }
 
-//a)
-public static void planningMatrix (){
+    //a)
+    public static void planningMatrix (){
         Scanner scanner = new Scanner(System.in);
 
         int quantityOfVehicles = scanner.nextInt();
@@ -16,58 +16,66 @@ public static void planningMatrix (){
         voltDeiMatrix = new int[quantityOfVehicles][quantityOfDays+1];
 
 
-    System.out.print("dia:");
-    for (int i = 0; i < quantityOfDays; i++) {
-        System.out.printf("%2d|", i);
-    }
-    System.out.println();
-
-    for (int i = 0; i < voltDeiMatrix.length; i++) {
-        voltDeiMatrix[i][0] = (i);
-        for (int j = 0; j < voltDeiMatrix[i].length ; j++) {
-
-            System.out.printf("%2d ", voltDeiMatrix[i][j]);
+        System.out.print("dia:");
+        for (int i = 0; i < quantityOfDays; i++) {
+            System.out.printf("%2d|", i);
         }
         System.out.println();
+
+        for (int i = 0; i < voltDeiMatrix.length; i++) {
+            voltDeiMatrix[i][0] = (i);
+            for (int j = 0; j < voltDeiMatrix[i].length ; j++) {
+
+                System.out.printf("%2d ", voltDeiMatrix[i][j]);
+            }
+            System.out.println();
+        }
     }
 
-    System.out.println(voltDeiMatrix);
-}
+    //b)
+    public static void totalTraveledDistance (int numberOfRows, int[][]voltDeiMatrix){
 
-//b)
-public static void totalTraveledDistance (){
+        int totalDistance = 0;
+        int[][] totalDistanceArray = new int[numberOfRows][2];
 
-}
-//c)
-public static void c (){}
+        for (int rowNumber = 1; rowNumber < voltDeiMatrix.length; rowNumber++) {
+            for (int columnNumber = 1; columnNumber < voltDeiMatrix[numberOfRows].length; columnNumber++) {
+                totalDistance += voltDeiMatrix[rowNumber][columnNumber];
+            }
 
-
-//d)
-public static void d (){}
-
-
-//e)
-public static void e (){}
-
-
-//f)
-public static void f (){}
+            totalDistanceArray[rowNumber - 1][2] = totalDistance;
+        }
+    }
+    //c)
+    public static void c (){}
 
 
-//g)
-public static void g (){}
+    //d)
+    public static void d (){}
 
 
-//h)
-public static void h (){}
+    //e)
+    public static void e (){}
 
 
-//i)
-public static void i (){}
+    //f)
+    public static void f (){}
 
 
-//j)
-public static void j (){}
+    //g)
+    public static void g (){}
+
+
+    //h)
+    public static void h (){}
+
+
+    //i)
+    public static void i (){}
+
+
+    //j)
+    public static void j (){}
 
 
 }
